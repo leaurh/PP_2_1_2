83a9e18352c62be915ec28ff0a8d8b3fa4dd446e
leaurh <arinamehrengina@yandex.ru>
2022-06-12 19:37:22 +0300

    solution commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean3.getMessage());
+
+        System.out.println(bean2 == bean3);
     }
 }
